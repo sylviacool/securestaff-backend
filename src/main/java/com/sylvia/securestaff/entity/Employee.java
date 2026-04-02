bcc72e123b88a8bbfbfeb99ad0febf99bddf81ae
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-02 12:17:58 +0200

    Add project structure and Employee JPA entity

---
+++ src/main/java/com/sylvia/securestaff/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.sylvia.securestaff.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String LastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private  String email;
+
+}
