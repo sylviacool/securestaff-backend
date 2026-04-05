2214c305b059ebec66f83b4e08aa139d5c8e4520
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-05 21:01:11 +0200

    Restructure project into backend and frontend folders

--- src/main/java/com/sylvia/securestaff/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sylvia.securestaff.repository;
-
-import com.sylvia.securestaff.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
