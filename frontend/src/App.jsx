5eb1d4d8975964f2efacf907180f78867b493cf8
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-08 00:00:15 +0200

    Create FooterComponent and Integrate FooterComponent into App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import FooterComponent from "./components/FooterComponent";
 
 const App = () => {
   return (
     <div>
       <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </div>
   );
 };
