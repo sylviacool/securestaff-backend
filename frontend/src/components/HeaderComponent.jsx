879b00817bf1a20d789297f6c5da364e0df76640
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-07 23:42:36 +0200

    Create HeaderComponent with basic layout

---
+++ frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="/">
+            SecureStaff
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
