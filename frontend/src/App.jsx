2bc025206e4f37693e070a1afdcf1f64b6ff9854
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-08 12:21:18 +0200

    Set up routing in App component using React Router and add route for ListEmployeeComponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,23 @@ import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import FooterComponent from "./components/FooterComponent";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/*//http://localhost:3000 */}
+            <Route path="/" element={<ListEmployeeComponent />}></Route>
+             {/*//http://localhost:3000/employees */}
+            <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
+     
     </div>
   );
 };
