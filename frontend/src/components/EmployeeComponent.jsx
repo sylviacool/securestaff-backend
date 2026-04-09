cf2c53a3e2d6379c924a5cb5106490c4cd299df0
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-09 18:37:35 +0200

    Add employee form with state handling and Bootstrap styling.

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const EmployeeComponent = () => {
-  return (
-    <div>
-      EmployeeComponent
-    </div>
-  )
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-export default EmployeeComponent
+  function handleFirstName(e) {
+    setFirstName(e.target.value);
+
+  }
+
+  
+  function handleLastName(e) {
+    setLastName(e.target.value);
+
+  }
+
+   function handleEmail(e) {
+    setEmail(e.target.value);
+
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card">
+          <h2 className="text-center">Add Employee</h2>
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-2">
+                <label className="form-label">First Name:</label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee First Name"
+                  name="firstName"
+                  value={firstName}
+                  className="form-control"
+                  onChange={handleFirstName}
+                />
+              </div>
+
+              <div className="form-group mb-2">
+                <label className="form-label">Last Name:</label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee Last Name"
+                  name="lastName"
+                  value={lastName}
+                  className="form-control"
+                  onChange={handleLastName}
+                />
+              </div>
+
+              <div className="form-group mb-2">
+                <label className="form-label">Email:</label>
+                <input
+                  type="text"
+                  placeholder="Enter Employee Email"
+                  name="Email"
+                  value={email}
+                  className="form-control"
+                  onChange={handleEmail}
+                />
+              </div>
+
+              <button className="btn btn-success" onClick={saveEmployee}>Submit</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeComponent;
--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   return (
     <div>
       <footer className="footer">
-        <span>All rights reserved 2026 by SylviaUzoma</span>
+        <span>All rights reserved 2026 by Sylvia Uzoma</span>
       </footer>
     </div>
   );
